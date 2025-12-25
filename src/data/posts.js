bfb4ec0fb09b84ac826a2d066f52598cc0f35cb0
ATzFauzan1525 <2400016068@webmail.uad.ac.id>
2025-12-25 22:34:49 +0700

    Update posts data - tambah konten blog

--- src/data/posts.js
+++ src/data/posts.js
@@ -4,16 +4,16 @@ export const posts = [
     slug: 'belajar-nextjs', 
     title: 'Belajar Next.js Dasar', 
     content: 'Next.js merupakan framework berbasis React yang sangat populer karena kemudahan dan performanya. Dengan dukungan seperti Server Components, sistem routing otomatis, serta optimasi bawaan, pengembangan aplikasi web menjadi lebih cepat dan terstruktur.',
-    author: 'Andi Pratama',
-    date: '2025-02-05'
+    author: 'Neil Melikah Fitri SUM',
+    date: '2025-10-05'
   },
   { 
     id: 2, 
     slug: 'routing-mudah', 
     title: 'Routing Sangat Mudah', 
     content: 'Next.js menerapkan konsep file-based routing, di mana setiap folder dan file page.jsx langsung menjadi sebuah route. Pendekatan ini menghilangkan kebutuhan konfigurasi routing yang kompleks seperti pada React Router.',
-    author: 'Dewi Lestari',
-    date: '2025-02-10'
+    author: 'Dewi Sartika',
+    date: '2025-10-10'
   },
   { 
     id: 3, 
@@ -21,6 +21,6 @@ export const posts = [
     title: 'Apa itu Server Components?', 
     content: 'Server Components memungkinkan proses rendering dilakukan di sisi server sebelum halaman dikirim ke browser. Teknik ini dapat mengurangi ukuran JavaScript yang dikirim ke client dan meningkatkan kecepatan serta efisiensi aplikasi.',
     author: 'Rizky Maulana',
-    date: '2025-02-15'
+    date: '2025-10-15'
   },
 ];
